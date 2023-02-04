e4a66076c42e0d24f006acb5350972c0a7efaa93
Yves Voirin <yves.voirin@geoimagesolutions.com>
2023-02-04 17:02:49 -0500

    fix directory name

--- utils/get_files_in_directory.py
+++ utils/get_files_in_directory.py
@@ -4,7 +4,7 @@ __author__ = 'voirin'
 # importer GLOB
 import glob
 # définir un répertoire à explorer
-directory_to_explore = r'C:\data'
+directory_to_explore = r'../data'
 # on récupère tous les fichiers du répertoire
 files = glob.glob(directory_to_explore + '\*.*')
 # on parcourt les fichiers trouvés
